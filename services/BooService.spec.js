01a1dcf98729d750cd72548da8173139f4e9ae31
Alan Cesar Elias <alan.elias@matera.com>
2017-11-30 14:05:49 -0200

    Primeiro teste

---
+++ services/BooService.spec.js
@@ -0,0 +1,12 @@
+/* global describe, it */
+const assert = require('assert');
+
+describe('BookService.js', () => {
+  it('Meu primeiro teste!', () => {
+    const a = 1;
+    const b = 2;
+    const c = a + b;
+
+    assert.equal(c, 3);
+  });
+});
