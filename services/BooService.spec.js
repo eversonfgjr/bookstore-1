e43c69fa3278872d096524cfa9da25744d053c61
Alan Cesar Elias <alan.elias@matera.com>
2017-11-30 14:19:16 -0200

    Teste BookService com done()

--- services/BooService.spec.js
+++ services/BooService.spec.js
@@ -23,4 +23,18 @@ describe('BookService.js', () => {
       assert.equal(book.title, 'The hitchhiker\'s guide to the galaxy');
     });
   });
+
+  it('Requisição válida e título do livro com done', (done) => {
+    // Given
+    const ISBN = '0345391802';
+
+    // When
+    const result = BookService.getBookByIsbn(ISBN);
+
+    // Then
+    result.then((book) => {
+      assert.equal(book.title, 'The hitchhiker\'s guide to the galaxy');
+      done(); // meu teste terminou;
+    });
+  });
 });
