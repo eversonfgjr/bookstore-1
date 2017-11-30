d8289af593838c19806e1fff008b03b97c796fa2
Alan Cesar Elias <alan.elias@matera.com>
2017-11-30 13:09:00 -0200

    Refatoração

--- services/BookService.js
+++ services/BookService.js
@@ -1,32 +1,35 @@
 const rp = require('request-promise');
+
 const API_KEY = 'SPNROBO0';
 
 class BookService {
-    getBookByIsbn(isbn) {
-        if (!isbn) {
-            throw new Error('ISBN deve ser informado!');
-        }
-
-        let options = {
-            uri: `http://isbndb.com/api/v2/json/${API_KEY}/book/${isbn}`,
-            json: true
-        };
-
-        return rp.get(options).then((response) => {
-            if (response.error) {
-                return {};
-            }
-
-            let { title, author_data, publisher_name, language } = response.data[0];
-
-            return {
-                title: title,
-                authors: author_data.map(author => author.name).join(';'),
-                publisher: publisher_name,
-                language: language,
-            };
-        });
+  static getBookByIsbn(isbn) {
+    if (!isbn) {
+      throw new Error('ISBN deve ser informado!');
     }
+
+    const options = {
+      uri: `http://isbndb.com/api/v2/json/${API_KEY}/book/${isbn}`,
+      json: true,
+    };
+
+    return rp.get(options).then((response) => {
+      if (response.error) {
+        return {};
+      }
+
+      const {
+        title, author_data, publisher_name, language,
+      } = response.data[0];
+
+      return {
+        title,
+        authors: author_data.map(author => author.name).join(';'),
+        publisher: publisher_name,
+        language,
+      };
+    });
+  }
 }
 
 module.exports = BookService;
